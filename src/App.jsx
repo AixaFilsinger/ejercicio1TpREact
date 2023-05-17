024833f57db81b10cfa8c0f2723863258749c571
Aixa Filsinger <macafil99@gmail.com>
2023-05-17 08:59:38 -0300

    se termino el ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Ejercicio1 from './Components/Ejercicio1'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   
 
   return (
     <>
-      
+      <Ejercicio1/>
     </>
   )
 }
---
+++ src/Components/Ejercicio1.jsx
@@ -0,0 +1,11 @@
+
+
+const Ejercicio1 = () => {
+    return (
+        <section className="text-center display-1 fw-bold">
+            <h1>Hello World!</h1>  
+        </section>
+    );
+};
+
+export default Ejercicio1;
